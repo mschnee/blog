58692d913446e0b841b66a1b9fb165984afe853e
Matthew Schnee <matthew.schnee@gmail.com>
2016-10-17 18:59:39 -0700

    updates for testing

--- server/Controllers/BlogController.ts
+++ server/Controllers/BlogController.ts
@@ -2,11 +2,17 @@ import { Request, Response } from 'express';
 import { IBlogController, BlogController as GeneratedBlogController } from '../../generated/services/BlogController';
 
 import { Blog } from '../../generated/types';
+const testPost: Blog.Post = {
+    postId: 'asdf',
+    title: 'So I was thining',
+    userId: '0000',
+    content: 'Some kind of lorem-ipsum, right?'
+}
 
 export class BlogController extends GeneratedBlogController implements IBlogController {
     public get(request: Request, response: Response, postId?: string): Blog.MultiPostResponse {
         const result: Blog.MultiPostResponse = {
-            posts: []
+            posts: [testPost]
         };
         
         return result;
